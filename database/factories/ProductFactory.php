fa7e6e7cb2ecc7ba83c7818f2fbc007da492e5dd
madi <120677197+kishikaisei69764@users.noreply.github.com>
2024-07-13 03:12:20 +0500

    Update ProductFactory.php

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -20,7 +20,11 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+       'title' => $this ->faker->sentence(3) ,
+      'description' =>$this ->faker->paragraph(1), 
+      'price' =>$this ->faker->randomFloat($maxDecimal = 2, $min = 3, $max = 100),
+      'stock' =>$this ->faker->numberBetween(1,10),
+      'status' =>$this ->faker->randomElement(['available','unavailable']),
         ];
     }
 }
