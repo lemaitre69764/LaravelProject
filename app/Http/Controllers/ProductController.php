5caa5e5099e791eb13e7026ebe5a4b8a6d57b4d2
madi <120677197+kishikaisei69764@users.noreply.github.com>
2024-07-14 01:42:24 +0500

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 class ProductController extends Controller
 {
     
     public function index()
     {
-        return view('products.index'); // test
+        $products = DB::table("products")->get();
+
+        dd($products);
+        return view('products.index');
     }
     public function create()
     {
