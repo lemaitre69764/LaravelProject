0a768aab8dc468ea22b2449ba8bdd49ed7d94630
madi <120677197+kishikaisei69764@users.noreply.github.com>
2024-07-16 21:55:41 +0500

    Update index.blade.php

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,38 +6,40 @@
   <title>Document</title>
 </head>
 <body>
-  <div class="table-responsive">
-  <table class="table table-striped">
-    <thead class="thead-light">
+  
 <h1>List Of Products</h1>
-<!-- @if (empty($products)) -->
- @empty ($products) 
+@if (empty($products)) 
 <div class="alert alert-warning">
   The list of products is empty
 </div>
 @else
-      <th>Id</th>
-      <th>Title</th>    
-      <th>Description</th>
-      <th>Price</th>
-      <th>Stock</th>
-      <th>Status</th>
-
-</thead>
-<tbody>
-  <tr>
-<td>Id</td>
-<td>Soap</td>
-<td>Best soap Decription</td>
-  </tr>
-  <tr>
-  <td>2</td>
-<td>Milk</td>
-<td>Description about milk</td>
-</tr>
-</tbody>
+<div class="table-responsive">
+  <table class="table table-striped">
+    <thead class="thead-light">
+      <tr>
+        <th>Id</th>
+        <th>Title</th>    
+        <th>Description</th>
+        <th>Price</th>
+        <th>Stock</th>
+        <th>Status</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($products as $product)
+      <tr>
+        <td>{{ $product->id }}</td>
+        <td>{{ $product->title }}</td>
+        <td>{{ $product->description }}</td>
+        <td>{{ $product->price }}</td>
+        <td>{{ $product->stock }}</td>
+        <td>{{ $product->status }}</td>
+      </tr>
+      @endforeach
+    </tbody>
   </table>
 </div>
+@endif
 
 </body>
 </html>
