44ce0e24d4b748a21e768b8267e085032756573a
madi <120677197+kishikaisei69764@users.noreply.github.com>
2024-07-16 22:22:55 +0500

    Update show.blade.php

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -5,7 +5,15 @@
         <title>Document</title>
     </head>
     <body>
-        <h1>fool</h1>
-        <p> </p>
+        <h1>{{ $product->title }} ({{ $product->id}})</h1>
+    {{--      {!! $subtitle !!} --}}
+    @{{ name }}
+        <p> {{ $product->description  }}</p> 
+        <p> {{ $product->price  }}</p>
+        <p> {{ $product->stock }}</p>
+        <p> {{ $product->stock  }}</p>
+        <p> {{ $product -> description }}</p>
+        <p> {{ $product -> description }}</p>
+        
     </body>
 </html> 
