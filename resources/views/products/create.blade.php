68176e8f5fb2bf80933d7bd8cf69f0c134b16f45
madi <120677197+kishikaisei69764@users.noreply.github.com>
2024-07-17 09:57:34 +0500

    Update create.blade.php

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,6 +1,23 @@
 @extends('layouts.master')
 @section('content')
      <h1>Create a product</h1>
-     <form method="POST"></form>
- @endsection
+     <form method="POST" action="{{ route ('products.store') }}"></form>
+ <div class="form-row">
+    <label>Title</label>
+<input class="form-control" type="text" name="title" required>
+</div>
+<div class="form-row">
+    <label>Description</label>
+<input class="form-control" type="text" name="title" required>
+</div>
+<div class="form-row">
+    <label>Priec</label>
+<input class="form-control" type="number" min="1.00" step="0.01" name="title" required>
+</div>
+<div class="form-row">
+    <label>Stock</label>
+<input class="form-control" type="number" min="0" name="title" required>
+</div>
+ 
+     @endsection
  
