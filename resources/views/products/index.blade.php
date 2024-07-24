06efea83978b90320a6a4462e9ade6779eacdc5c
madi <120677197+kishikaisei69764@users.noreply.github.com>
2024-07-24 21:07:36 +0500

    Update index.blade.php

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.master')
 @section('content')
 <h1>List Of Products</h1>
-@if (empty($products)) 
+<a class="btn btn-success" href="{{ route('products.create') }}">Create</a>
+
+@empty($products)
 <div class="alert alert-warning">
   The list of products is empty
 </div>
@@ -27,6 +29,11 @@
         <td>{{ $product->price }}</td>
         <td>{{ $product->stock }}</td>
         <td>{{ $product->status }}</td>
+        <td>
+        <a class="btn btn-link" href="{{ route('products.show', ['product' => $product->id]) }}">Show</a>
+        <a class="btn btn-link" href="{{ route('products.edit', ['product' => $product->id]) }}">Edit</a>
+        </td>
+          
       </tr>
       @endforeach
     </tbody>
